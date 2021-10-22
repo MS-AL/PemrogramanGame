cae090f6c06b94aa582cb93b2c7199e424704611
MS-AL <rizqir25@gmail.com>
2021-10-22 15:14:27 +0700

    GameShooter

---
+++ Game Shooter/Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col){
+        if (col.gameObject.CompareTag("Player")){
+            WeaponController.ammoMag = 90;
+        }
+        
+    }
+}
