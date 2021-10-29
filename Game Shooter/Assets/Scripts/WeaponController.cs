9fce5c383164197f61dc7c640b5805e9224936bb
MS-AL <rizqir25@gmail.com>
2021-10-29 14:35:43 +0700

    Game Shooter

--- Game Shooter/Assets/Scripts/WeaponController.cs
+++ Game Shooter/Assets/Scripts/WeaponController.cs
@@ -19,6 +19,9 @@ public class WeaponController : MonoBehaviour
         public static int ammoMag = 90;
         public Slider healthBar;
         [SerializeField] Camera FPSCamera, TPSCamera;
+    public AudioSource AK47Shot;
+    public AudioSource ShotgunShot;
+    public AudioSource WeaponReload;
 
     // Start is called before the first frame update
     void Start()
@@ -39,11 +42,24 @@ public class WeaponController : MonoBehaviour
             if(ammoMag != 0) 
             {
                 trigger = false;
+                WeaponReload.Play();
                 StartCoroutine(waitReload());
             }
             else
             {
                 Debug.Log("Peluru Habis");
+            }
+        }
+        if (AIEnemyController.GiveDamage == true)
+        {
+            health -= AIEnemyController.EnemyDamage;
+            healthBar.value = health;
+            if (health <= 0)
+            {
+                Debug.Log("Player Mati");
+                SceneManager.LoadScene("Game Over");
+                AIEnemyController.GiveDamage = false;
+                Restart();
             }
         }
     }
@@ -79,6 +95,8 @@ public class WeaponController : MonoBehaviour
     {
         if (ammo != 0)
         {
+            AK47Shot.Play();
+            ShotgunShot.Play();
             RaycastHit hit;
             Ray ray = new Ray(transform.position, transform.forward);
             if (Physics.Raycast(ray, out hit, range))
